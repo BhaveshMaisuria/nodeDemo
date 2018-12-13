d8e9306e92eb98edcceb58dc84916b35228eecea
Bhavesh Maisuria <bhaveshmaisuria@jacksolutions.co>
2018-12-13 11:12:55 +0530

    test for jenkins

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(helmet());
-
+console.log(helmet());
 var enableCORS = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
