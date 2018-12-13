700bf183bfe146fd49ef531a9f3d85dc51134920
Bhavesh Maisuria <bhaveshmaisuria@jacksolutions.co>
2018-12-13 11:29:56 +0530

    console add

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(bodyParser.urlencoded({
 app.use(helmet());
 console.log('helmet');
 console.log(helmet());
+console.log('end');
 var enableCORS = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
